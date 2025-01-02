3c3428915e1cbd6a7e450e0ba6b30d58a457187d
Chirag Goel <engineerchirag021@gmail.com>
2025-01-02 21:03:42 +0530

    Class 4

---
+++ Class4/f2.js
@@ -0,0 +1,47 @@
+// Example1
+// console.log(a);
+// var a = 1;
+// ----
+
+// var a;
+// console.log(a);
+// a = 1;
+
+
+// Example2
+// console.log(sum(1, 2, 3));
+
+// function sum(a, b, c) {
+//     let total = a + b + c;
+//     return total;
+// }
+// ------
+
+// function sum(a, b, c) {
+//     let total = a + b + c;
+//     return total;
+// }
+
+// console.log(sum(1, 2, 3));
+
+
+// Example3
+// console.log(sum1(1, 2, 3));
+
+// var sum1 = function(a, b, c) {
+//     let total = a + b + c;
+//     return total;
+// }
+
+// -----
+
+// var sum1;
+
+// console.log(sum1);
+// console.log(sum1(1, 2, 3));
+
+// sum1 = function(a, b, c) {
+//     let total = a + b + c;
+//     return total;
+// }
+
